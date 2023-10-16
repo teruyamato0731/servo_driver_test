c6806787fac4862fdedbd3c310db62d3bd8b7f8d
Yoshikawa Teru <77840326+teruyamato0731@users.noreply.github.com>
2023-10-16 18:48:02 +0900

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <mbed.h>
+
+// Const variable
+
+// Function prototype
+
+// IO
+// CAN can{PA_11, PA_12, (int)1e6};
+// CAN can{PB_12, PB_13, (int)1e6};
+// CANMessage msg;
+
+// Struct definition
+
+// Global variable
+
+/// @brief The application entry point.
+int main() {
+  // put your setup code here, to run once:
+  printf("\nsetup\n");
+  while(1) {
+    // put your main code here, to run repeatedly:
+    auto now = HighResClock::now();
+    static auto pre = now;
+    if(now - pre > 20ms) {
+      printf("hoge\n");
+      pre = now;
+    }
+  }
+}
+
+// Function definition

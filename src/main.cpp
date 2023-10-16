9963fd40c25af830063c597128cb531223d9fd10
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-10-16 19:10:54 +0900

    feat: とりあえずのコードを作成

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <mbed.h>
 
 // Const variable
+constexpr uint32_t servo_can_id = 140;
 
 // Function prototype
 
 // IO
-// CAN can{PA_11, PA_12, (int)1e6};
+CAN can{PA_11, PA_12, (int)1e6};
 // CAN can{PB_12, PB_13, (int)1e6};
 // CANMessage msg;
 
 // Struct definition
+struct Servo {
+  CANMessage to_msg() {
+    return CANMessage{can_id, pwm, sizeof(pwm)};
+  }
+
+  uint32_t can_id;
+  uint8_t pwm[8];
+};
 
 // Global variable
+Servo servo{servo_can_id};
 
 /// @brief The application entry point.
 int main() {
@@ -22,7 +32,10 @@ int main() {
     auto now = HighResClock::now();
     static auto pre = now;
     if(now - pre > 20ms) {
-      printf("hoge\n");
+      const auto msg = servo.to_msg();
+      bool success = can.write(msg);
+      if(!success) printf("fail ");
+      printf("\n");
       pre = now;
     }
   }
